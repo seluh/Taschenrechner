31979c8f0fbd6e381af7146d10e58bdd62e340a2
seluh <joseflu@edu.aau.at>
2017-03-14 18:33:21 +0100

    Ausgabe von Kommazahlen implementiert Reset der Felder nach division Angepasstes Layout Explizites Handling bei 0

--- app/src/main/java/com/example/seluh/taschenrechner/MainActivity.java
+++ app/src/main/java/com/example/seluh/taschenrechner/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.seluh.taschenrechner;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,23 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
-    public void divide (View view){
+
+    public void divide(View view) {
+
         EditText z1 = (EditText) findViewById(R.id.zahl1);
         EditText z2 = (EditText) findViewById(R.id.zahl2);
         EditText z3 = (EditText) findViewById(R.id.ergebnis);
 
-        int a = Integer.parseInt(z1.getText().toString());
-        int b = Integer.parseInt(z2.getText().toString());
+        double a = Integer.parseInt(z1.getText().toString());
+        double b = Integer.parseInt(z2.getText().toString());
+        if (b == 0) {
+            z3.setText("Divisor should not be 0");
+        } else {
+            try {
+                double c = a / b;
+                z3.setText(String.valueOf(c));
 
-        try {
-            int c = a / b;
-            z3.setText(String.valueOf(c));
+            } catch (Exception e) {
+
+                z3.setText("ERROR!");
+            }
         }
-        catch (Exception e){
-            z3.setText("ERROR!");
-        }
-
-
+        z1.setText("");
+        z2.setText("");
     }
+
 }
